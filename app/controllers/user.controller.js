4d3ddc85831017c568ffcbbc84e95fcfe57dc125
Alivia-Gubb <alivia.gubb@proton.me>
2024-07-01 10:54:48 +0100

    Fetches userName

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,7 +1,15 @@
-const { getAllUsers } = require("../models/user.model");
+const { getAllUsers, fetchUserName } = require("../models/user.model");
+
 
 exports.getUsers = (req, res) => {
   getAllUsers().then((users) => {
     res.status(200).json(users);
   });
 };
+
+exports.getUserName = (req, res) => {
+  fetchUserName(req.params.username)
+  .then((user) => {
+    res.status(200).send({ user })
+  })
+}
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -17,3 +17,25 @@ exports.getAllUsers = () => {
     }
   });
 };
+const fetchUserName =(userName) => {
+  if(!(userName)){
+    return Promise.reject({
+      status : 400,
+      msg: 'Bad Request'
+    });
+  }
+  const userName = ref(db, `users/${userName}`)
+
+  return get(query(userName)).then((data) => {
+    if(data.exists()){
+      return data
+        } else {
+          return {}
+        }
+  })
+
+}
+
+module.exports {fetchUserName}
+
+
